a2381f895aedf72ffd838be92a67369872c75333
barikaricky <barikaricky@gmail.com>
2026-01-07 05:04:17 -0800

    Add new icon images for frontend application
    - Added icon-72x72.png and icon-96x96.png to the frontend assets.
    - These images will be used for application branding and display purposes.

--- apps/frontend/dev-dist/sw.js
+++ apps/frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-a731ab65'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.kk5cq8dujc8"
+    "revision": "0.dnitsiok7d8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
